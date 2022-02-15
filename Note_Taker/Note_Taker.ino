7e8740f99e2db75fffd52b3f12dbf22dc30da7ce
J <joshua.lea@icloud.com>
2022-02-15 18:05:48 +1100

    Update Note_Taker.ino
    Add min and max value constants, move checkrotary to Rotary class, menu looping

--- Note_Taker/Note_Taker.ino
+++ Note_Taker/Note_Taker.ino
@@ -1,6 +1,11 @@
+#define minvalue 0
+#define maxvalue 10
+
 class Rotary {
   private:
-    int checkrotary(int rotary1, int rotary2){
+    float s=0;
+    
+    float checkrotary(int rotary1, int rotary2){
     static byte old=0;
     byte new1 = digitalRead(rotary1);
     byte new2 = digitalRead(rotary2);
@@ -8,23 +13,22 @@ class Rotary {
     byte d = old-new_var;
     old=new_var;
     switch(d){
-      case 1: return 1;
-      case 3: return -1;
-      case 255: return -1;
-      case 253: return 1;
+      case 1: return -0.25;
+      case 3: return 0.25;
+      case 255: return 0.25;
+      case 253: return -0.25;
       default: return 0;
       }
     }
+
   public:
-    int s=0;
 
     void printRotary() {
       while (true){
-        int d = checkrotary(6, 7);
+        float d = checkrotary(6, 7);
         s=s+d;
-        int t=(s/4);
   
-        if(d){Serial.println(t);}
+        if(d){Serial.println(int(s));}
       }
     }
 };
@@ -40,20 +44,4 @@ void setup() {
 
 void loop() {
 
-}
-
-int checkrotary(int rotary1, int rotary2){
-  static byte old=0;
-  byte new1 = digitalRead(rotary1);
-  byte new2 = digitalRead(rotary2);
-  byte new_var = (new1*2)|(new2^new1);
-  byte d = old-new_var;
-  old=new_var;
-  switch(d){
-    case 1: return 1;
-    case 3: return -1;
-    case 255: return -1;
-    case 253: return 1;
-    default: return 0;
-  }
 }
